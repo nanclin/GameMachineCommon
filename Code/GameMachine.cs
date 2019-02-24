4b61ce9ec6d08f428fb7d6fdf644777107f5eff8
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2019-02-24 11:58:13 +0100

    Implemented game list & opening loading game scene.

---
+++ Code/GameMachine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameMacine {
+    
+    public class GameMachine : MonoBehaviour {
+
+        public void LoadGame(string sceneName) {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+}
