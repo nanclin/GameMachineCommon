b36b3c179788db45a4436c3139c25b0f1162b9d1
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2019-02-24 12:24:29 +0100

    Added game base script & exit button prefab.

---
+++ Code/GameBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameMachine {
+    
+    public class GameBase : MonoBehaviour {
+
+        public void ExitGame() {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+}
